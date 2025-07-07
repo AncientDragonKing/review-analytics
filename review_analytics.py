008063e97c3936cdf40f96b389bc5adade536ff4
Nereus Chen <nereuschen0122@gmail.com>
2025-07-07 21:59:03 +0800

    New File

---
+++ review_analytics.py
@@ -0,0 +1,10 @@
+# review(留言)-analytics(分析)
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(data)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取玩了，總共有', len(data), '筆留言')
